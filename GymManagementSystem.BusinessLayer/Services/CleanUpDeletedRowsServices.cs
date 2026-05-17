3492e69c410b76aca4a6849af2eaf6e1a6e4555d
lamonaDev <aymanmohamed.programmer@gmail.com>
2026-05-18 00:45:46 +0300

    Add service that delete marked as deleted rows each 30 days

---
+++ GymManagementSystem.BusinessLayer/Services/CleanUpDeletedRowsServices.cs
@@ -0,0 +1,29 @@
+﻿using GymManagementSystem.BusinessLayer.Interfaces;
+using GymManagementSystem.DataLayer.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagementSystem.BusinessLayer.Services;
+/// <summary>
+/// the main logic to clean up deleted rows
+/// </summary>
+public class CleanUpDeletedRowsServices : ICleanUpDeletedRows
+{
+    private readonly GymDatabaseContext _context;
+    public CleanUpDeletedRowsServices(GymDatabaseContext context)
+    {
+        _context = context;
+    }
+    public async Task<int> SoftDeleteAsync(CancellationToken ct = default)
+    {
+        var numberOfDeletedRows = 0;
+        numberOfDeletedRows += await _context.Members.IgnoreQueryFilters().Where(e => e.IsDeleted).ExecuteDeleteAsync(ct);
+        numberOfDeletedRows += await _context.Trainers.IgnoreQueryFilters().Where(e => e.IsDeleted).ExecuteDeleteAsync(ct);
+        numberOfDeletedRows += await _context.Sessions.IgnoreQueryFilters().Where(e => e.IsDeleted).ExecuteDeleteAsync(ct);
+        numberOfDeletedRows += await _context.Bookings.IgnoreQueryFilters().Where(e => e.IsDeleted).ExecuteDeleteAsync(ct);
+        numberOfDeletedRows += await _context.HealthRecords.IgnoreQueryFilters().Where(e => e.IsDeleted).ExecuteDeleteAsync(ct);
+        numberOfDeletedRows += await _context.Plans.IgnoreQueryFilters().Where(e => e.IsDeleted).ExecuteDeleteAsync(ct);
+        numberOfDeletedRows += await _context.Categories.IgnoreQueryFilters().Where(e => e.IsDeleted).ExecuteDeleteAsync(ct);
+        numberOfDeletedRows += await _context.Memberships.IgnoreQueryFilters().Where(e => e.IsDeleted).ExecuteDeleteAsync(ct);
+        return numberOfDeletedRows;
+    }
+}
---
+++ GymManagementSystem.PresentationLayer/BackgroundGobs/SoftDeleteCleanUp.cs
@@ -0,0 +1,38 @@
+﻿using GymManagementSystem.BusinessLayer.Interfaces;
+using GymManagementSystem.BusinessLayer.Services;
+
+namespace GymManagementSystem.PresentationLayer.BackgroundGobs;
+/// <summary>
+/// main execution of the remove soft delete rows in main database logic using the DI container
+/// </summary>
+public class SoftDeleteCleanUp : BackgroundService
+{
+    private readonly IServiceScopeFactory _scopeFactory;
+    private readonly ILogger<CleanUpDeletedRowsServices> _logger;
+
+    public SoftDeleteCleanUp(IServiceScopeFactory scopeFactory,
+        ILogger<CleanUpDeletedRowsServices> logger
+        )
+    {
+        _scopeFactory = scopeFactory;
+        _logger = logger;
+    }
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            try
+            {
+                await using var scope = _scopeFactory.CreateAsyncScope();
+                var cleanUpDeletedRows = scope.ServiceProvider.GetService<ICleanUpDeletedRows>();
+                var numberOfDeletedRows = await cleanUpDeletedRows?.SoftDeleteAsync(stoppingToken)!;
+                _logger.LogInformation($"numberOfDeletedRows: {numberOfDeletedRows} from the CleanUpDeletedRows service");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Clean Up Deleted Rows Service Exception");
+            }
+            await  Task.Delay(TimeSpan.FromDays(30), stoppingToken);
+        }
+    }
+}
